30a90c5cad43313354b20cda9e4850f5aed39c80
maacrei <maacrei5723@gmail.com>
2022-05-11 08:03:59 +0000

    [Add]chapter7c

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,6 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user!
-  before_action :ensure_corrent_user, only: [:edit, :update]
+  before_action :ensure_current_user, only: [:edit, :update]
 
   def index
     @book = Book.new
--- app/models/group.rb
+++ app/models/group.rb
@@ -5,4 +5,8 @@ class Group < ApplicationRecord
   validates :name, presence: true
   validates :introduction, presence: true
   has_one_attached :image
+
+  def get_image(width,height)
+    image.variant(resize_to_limit: [width, height]).processed
+  end
 end
